f039a362ca1a94faf99f4aa09f5e65ce46f08170
miguel-cardoso08k <joaomiguel.cardoso2006@outlook.com>
2026-05-04 21:37:42 -0300

    chore: format prisma module

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,18 +3,20 @@ import { PrismaClient } from 'generated/prisma/client.js';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor() {
     const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
     super({ adapter });
   }
 
-    async onModuleInit() {
-        await this.$connect();
-    }
+  async onModuleInit() {
+    await this.$connect();
+  }
 
-    async onModuleDestroy() {
-        await this.onModuleDestroy();
-    }
-
+  async onModuleDestroy() {
+    await this.onModuleDestroy();
+  }
 }
