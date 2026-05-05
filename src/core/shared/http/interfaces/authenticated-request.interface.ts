8efb9d48f1622779c74e6584f7c4b7e20bc63ce0
miguel-cardoso08k <joaomiguel.cardoso2006@outlook.com>
2026-05-04 21:27:35 -0300

    feat: add shared auth request helpers

---
+++ src/core/shared/http/interfaces/authenticated-request.interface.ts
@@ -0,0 +1,9 @@
+import { FastifyRequest } from 'fastify';
+import { AuthenticatedUserEntity } from 'src/module/auth/domain/entities/authenticated-user.entity';
+import { RequestContext } from './request-context.interface';
+
+export interface AuthenticatedRequest extends FastifyRequest {
+  user?: AuthenticatedUserEntity;
+  token: string;
+  context: RequestContext;
+}
