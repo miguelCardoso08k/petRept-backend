819b512ea1fa71117731b53a8c1b3de17dd4a648
miguel-cardoso08k <joaomiguel.cardoso2006@outlook.com>
2026-05-13 06:31:59 -0300

    test: fix auth and user spec providers

--- src/module/auth/application/auth.service.spec.ts
+++ src/module/auth/application/auth.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UserRepository } from '../../user/domain/repositories/user.repository';
+import { PasswordHasherRepository } from '../domain/repositories/password-hasher.repository';
+import { TokenRepository } from '../domain/repositories/token.repository';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: PasswordHasherRepository, useValue: {} },
+        { provide: UserRepository, useValue: {} },
+        { provide: TokenRepository, useValue: {} },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
